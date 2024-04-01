a6c3ec873b4c27825b662331597dc4c97afb52e8
AnikaGG <anikug@hotmail.com>
2024-04-01 11:50:12 -0700

    merging after db

--- LodeLMmvn/src/main/java/activities/ClientHandler.java
+++ LodeLMmvn/src/main/java/activities/ClientHandler.java
@@ -6,8 +6,6 @@ import java.util.Base64;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import utils.*;
 
